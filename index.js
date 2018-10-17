6e4b78759ee1d3c1dca082b61519bbffd6f567bc
Monty Khanna <monty.khanna@simsaw.com>
2018-10-17 18:03:48 +0530

    Updated index file and removed packages

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-import React from 'react'; // eslint-disable-line
-import PropTypes from 'prop-types';
+'use strict';
 
-const Markup = ({ htmlTag, htmlString, ...rest }) => {
-  const custom = rest || {};
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; } // eslint-disable-line
+
+
+var Markup = function Markup(_ref) {
+  var htmlTag = _ref.htmlTag,
+    htmlString = _ref.htmlString,
+    rest = _objectWithoutProperties(_ref, ['htmlTag', 'htmlString']);
+  
+  var custom = rest || {};
   custom.dangerouslySetInnerHTML = { __html: htmlString };
-  return React.createElement(htmlTag, custom);
+  return _react2.default.createElement(htmlTag, custom);
 };
 
 Markup.propTypes = {
-  htmlTag: PropTypes.string,
-  htmlString: PropTypes.string
+  htmlTag: _propTypes2.default.string,
+  htmlString: _propTypes2.default.string
 };
 
 Markup.defaultProps = {
